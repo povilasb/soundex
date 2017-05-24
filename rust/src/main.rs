0cc32601fff17e1851dec09e1f9661449d666019
Povilas Balciunas <balciunas90@gmail.com>
2017-05-24 21:46:02 +0300

    Borrow strings instead of taking ownership

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,16 +6,16 @@ use std::collections::HashMap;
 
 // TODO: return iterator instead of string.
 // Now I couldn't make the function return an iterator...
-fn remove_letters_(word: String, rm_us: &[char]) -> String {
+fn remove_letters_(word: &String, rm_us: &[char]) -> String {
     return word.chars().filter(|x| !rm_us.contains(x)).collect()
 }
 
-fn rm_hw(word: String) -> String {
-    return remove_letters_(word, &['h', 'H', 'w', 'W']);
+fn rm_hw(word: &String) -> String {
+    return remove_letters_(&word, &['h', 'H', 'w', 'W']);
 }
 
-fn rm_vowels(word: String) -> String {
-    return remove_letters_(word, &['a', 'e', 'i', 'o', 'u', 'y', 'h',
+fn rm_vowels(word: &String) -> String {
+    return remove_letters_(&word, &['a', 'e', 'i', 'o', 'u', 'y', 'h',
         'A', 'E', 'I', 'O', 'U', 'Y', 'H']);
 }
 
@@ -26,7 +26,7 @@ fn tr_char(letter: char, map: &HashMap<char, char>) -> char {
     }
 }
 
-fn unique_str(word: String) -> String {
+fn unique_str(word: &String) -> String {
     let mut uniq = "".to_string();
     let mut last_char = '\0';
     for c in word.chars() {
@@ -38,7 +38,7 @@ fn unique_str(word: String) -> String {
     return uniq;
 }
 
-fn soundex(word: String) -> String {
+fn soundex(word: &String) -> String {
     let map: HashMap<_, _> = [
             ('b', '1'), ('f', '1'), ('p', '1'), ('v' , '1'),
             ('c', '2'), ('g', '2'), ('j', '2'), ('k', '2'), ('q', '2'),
@@ -46,28 +46,28 @@ fn soundex(word: String) -> String {
             ('d', '3'), ('t', '3'),
             ('l', '4'), ('m', '5'), ('n', '5'), ('r', '6'),
         ].iter().cloned().collect();
-    let suffix = word.chars().skip(1).collect();
+    let suffix: String = word.chars().skip(1).collect();
     let first_letter: String = word.chars().take(1).collect();
     return (first_letter + &rm_vowels(
-        unique_str(
-            rm_hw(suffix).chars().map(|c| tr_char(c, &map)).collect()
+        &unique_str(
+            &rm_hw(&suffix).chars().map(|c| tr_char(c, &map)).collect()
         )
     )).chars().take(4).collect();
 }
 
 fn main() {
     let word = String::from("Ashcroft");
-    println!("{}", soundex(word));
+    println!("{}", soundex(&word));
 }
 
 #[test]
 fn rm_hw_removes_h_and_w_in_any_case() {
-    assert!(rm_hw("haHbwcWd".to_string()) == "abcd")
+    assert!(rm_hw(&"haHbwcWd".to_string()) == "abcd")
 }
 
 #[test]
 fn rm_vowels_removes_them_in_any_case() {
-    assert!(rm_vowels("aAbcdiI".to_string()) == "bcd")
+    assert!(rm_vowels(&"aAbcdiI".to_string()) == "bcd")
 }
 
 #[test]
@@ -92,14 +92,14 @@ fn tr_char_returns_the_same_character_if_it_does_not_exist_in_the_dictionary() {
 
 #[test]
 fn unique_str_returns_string_with_no_repeating_adjacent_characters() {
-    let new_str = unique_str("aabccdee".to_string());
+    let new_str = unique_str(&"aabccdee".to_string());
 
     assert!(new_str == "abcde");
 }
 
 #[test]
 fn soundex_returns_max_4_symbols() {
-    assert!(soundex("Abcdl".to_string()) == "A123");
+    assert!(soundex(&"Abcdl".to_string()) == "A123");
 }
 
 #[test]
@@ -110,9 +110,9 @@ fn soundex_appends_zeroes_when_string_is_too_short() {
 
 #[test]
 fn soundex_encodes_word_using_soundex_algorithm() {
-    assert!(soundex("Ashcraft".to_string()) == "A261");
-    assert!(soundex("Ashcroft".to_string()) == "A261");
-    assert!(soundex("Tymczak".to_string()) == "T522");
+    assert!(soundex(&"Ashcraft".to_string()) == "A261");
+    assert!(soundex(&"Ashcroft".to_string()) == "A261");
+    assert!(soundex(&"Tymczak".to_string()) == "T522");
     // TODO
     //assert!(soundex("Pfister".to_string()) == "P263");
 }
